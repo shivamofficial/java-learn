31db41512aae274955dc7eef910c6d0ad0b62942
shivamofficial <91952992+shivamofficial@users.noreply.github.com>
2022-10-15 09:30:45 +0530

    Create error.java

---
+++ error.java
@@ -0,0 +1,20 @@
+
+public class Main
+{
+   public void add()
+    {
+        int a=10;
+        int b=20;
+        int c=a+b;
+      System.out.println(c);
+    }
+
+
+    
+public static void main (String[] args)
+{
+    
+    Main.obj=new Main();
+        obj.add();
+    }
+}
