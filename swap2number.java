5263e61687bea18272ccdfb11b0ccd57ccfe889d
shivamofficial <91952992+shivamofficial@users.noreply.github.com>
2022-10-15 21:07:31 +0530

    Create swap2number.java

---
+++ swap2number.java
@@ -0,0 +1,16 @@
+//import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	
+	//Scanner sc = new Scanner(System.in);
+	
+int a=10;
+int b=20;
+int temp;
+temp=a;
+a=b;
+b=temp;
+System.out.println("swap of 2 number is "+a+" " +b);
+	}
+}
