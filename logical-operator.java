329972be219cd0f4c694b706145a78743cadd508
shivamofficial <91952992+shivamofficial@users.noreply.github.com>
2022-10-15 21:55:43 +0530

    Create logical-operator.java

---
+++ logical-operator.java
@@ -0,0 +1,16 @@
+public class Main
+{
+	public static void main(String[] args) {
+		
+int a=10;
+int b=20;
+int c=30;
+
+
+
+System.out.println((b>a) && (c>b)); // true
+System.out.println((b>a) || (c>b)); // true
+		
+		
+	}
+}
