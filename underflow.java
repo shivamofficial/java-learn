e55ad829cefc90250721faac9a68b94ec3d6a32a
shivamofficial <91952992+shivamofficial@users.noreply.github.com>
2022-10-15 16:51:00 +0530

    Create underflow.java

---
+++ underflow.java
@@ -0,0 +1,8 @@
+class Simple{  
+public static void main(String[] args){  
+ 
+int a=-21474836467;  // range
+int y=a-1;  // range sai ek kam ho gya to underflow
+System.out.println(a);  
+System.out.println(y);  
+}}
