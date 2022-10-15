7482697135e2d0eb9e3e6b8b921f15afaa288254
shivamofficial <91952992+shivamofficial@users.noreply.github.com>
2022-10-15 21:49:15 +0530

    Create if_else.java

---
+++ if_else.java
@@ -0,0 +1,14 @@
+public class Main
+{
+	public static void main(String[] args) {
+		
+		char ch='a';
+		if(ch >70)
+		
+		  System.out.println("condition true"); // condition true (because AcII value of a is 97)
+		 
+		
+		else
+		System.out.println("condition false");
+	}
+}
