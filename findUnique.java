df7335abee8fe9faabd788ebba50b6f206244295
shivam kumar <91952992+shivamofficial@users.noreply.github.com>
2023-11-15 23:18:28 +0530

    Create findUnique.java

---
+++ findUnique.java
@@ -0,0 +1,15 @@
+
+
+public class Solution{  
+
+    public static int findUnique(int[] arr){
+		//Your code goes here
+    int n =arr.length;
+    int ans=0;
+for(int i=0;i<n;i++)
+{
+ans=ans^arr[i];
+}
+return ans;
+    }
+}
